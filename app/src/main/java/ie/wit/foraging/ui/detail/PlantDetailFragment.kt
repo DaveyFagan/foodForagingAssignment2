a76fa5ec31c8c86b269a99222c9fdb600864895f
David Fagan <david.fagan@diona.com>
2022-12-22 13:36:38 +0000

    Add date picker to edit

--- app/src/main/java/ie/wit/foraging/ui/detail/PlantDetailFragment.kt
+++ app/src/main/java/ie/wit/foraging/ui/detail/PlantDetailFragment.kt
@@ -1,5 +1,6 @@
 package ie.wit.foraging.ui.detail
 
+import android.app.DatePickerDialog
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.view.*
@@ -9,10 +10,13 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import ie.wit.foraging.R
+import ie.wit.foraging.databinding.FragmentForagingBinding
 import ie.wit.foraging.databinding.FragmentPlantDetailBinding
 import ie.wit.foraging.ui.auth.LoggedInViewModel
 import ie.wit.foraging.ui.foragingList.ForagingListViewModel
 import timber.log.Timber
+import java.util.*
 
 class PlantDetailFragment : Fragment() {
 
@@ -34,6 +38,8 @@ class PlantDetailFragment : Fragment() {
         plantDetailViewModel = ViewModelProvider(this).get(PlantDetailViewModel::class.java)
         plantDetailViewModel.observablePlant.observe(viewLifecycleOwner, Observer { render() })
 
+        setDateListener()
+
         fragBinding.editPlantButton.setOnClickListener {
             plantDetailViewModel.updatePlant(loggedInViewModel.liveFirebaseUser.value?.uid!!,
                 args.foragingid, fragBinding.foragingvm?.observablePlant!!.value!!)
@@ -51,9 +57,8 @@ class PlantDetailFragment : Fragment() {
     private fun render() {
         fragBinding.editScientificName.setText("A Message")
         fragBinding.editCommonName.setText("")
-        fragBinding.editScientificName.setText("")
+        fragBinding.editDatePlantPicked.setText("")
         fragBinding.foragingvm = plantDetailViewModel
-        Timber.i("Retrofit fragBinding.donationvm == $fragBinding.foragingvm")
     }
 
     override fun onResume() {
@@ -66,6 +71,28 @@ class PlantDetailFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _fragBinding = null
+    }
+
+    fun setDateListener() {
+        fragBinding.editDatePlantPicked.setOnClickListener {
+
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+            val datePickerDialog = DatePickerDialog(
+
+                requireContext(), R.style.DialogTheme,
+                { view, year, monthOfYear, dayOfMonth ->
+                    val dat = (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                    fragBinding.editDatePlantPicked.setText(dat)
+                },
+                year,
+                month,
+                day
+            )
+            datePickerDialog.show()
+        }
     }
 
 }
